044053b4b62ebe3aeccf2a9b3f8e6664cabe8d3e
T1mpp4 <timi.rautamaki@gmail.com>
2019-09-23 19:31:21 +0200

    Add Snapdragon applications

--- common.mk
+++ common.mk
@@ -142,7 +142,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+
+PRODUCT_PACKAGES += \
+    SnapdragonGallery \
+    SnapdragonMusic \
+    SnapdragonCamera \
+    MusicFX
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
