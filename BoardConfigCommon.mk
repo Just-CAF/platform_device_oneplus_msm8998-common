bcc2df7c6bc4527ea64e8c78a827a8d0054bd05e
T1mpp4 <timi.rautamaki@gmail.com>
2019-09-26 10:27:18 +0200

    Add minimal sepolicy, disable seclabel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -291,8 +291,8 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 
 # SELinux
-include device/qcom/sepolicy/Android.mk
-
+#include device/qcom/sepolicy/Android.mk
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-minimal
 #BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
