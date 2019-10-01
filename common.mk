10a2cd3c93526aba1323b85a0e958dfb4673f3ad
Pranav Vashi <neobuddy89@gmail.com>
2019-10-01 23:05:45 +0530

    msm8998-common: Update widevine hidl to 1.2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -209,8 +209,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.1-service.widevine
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.2-service.widevine
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
