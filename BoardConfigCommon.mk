1e6e0adbdad596ff4f5637b45c1322a28b8a79c3
T1mpp4 <timi.rautamaki@gmail.com>
2019-10-15 16:21:03 +0200

    Include sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -293,11 +293,11 @@ BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 
 # SELinux
 #include device/qcom/sepolicy-legacy-um/sepolicy.mk
-#include device/qcom/sepolicy/Android.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-minimal
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-minimal
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
