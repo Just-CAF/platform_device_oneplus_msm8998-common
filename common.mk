745fa2789eb32dfc365c086d0ad02191a1358a52
T1mpp4 <timi.rautamaki@gmail.com>
2019-11-18 20:03:22 +0100

    msm8998-common: add few bt packages

--- common.mk
+++ common.mk
@@ -151,7 +151,9 @@ PRODUCT_PACKAGES += \
     bt-mac-generator \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    libbluetooth_qti \
+    libbt-logClient.so
 
 # Camera
 PRODUCT_PACKAGES += \
