82be531570bfe6f1e24bc9640ac4c0184cf1310a
T1mpp4 <timi.rautamaki@gmail.com>
2019-11-19 07:00:09 +0100

    msm8998-common: add android.hardware.audio@5.0

--- common.mk
+++ common.mk
@@ -98,6 +98,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
