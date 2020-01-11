089a58ab108b4efbd79b73d21f9d0b056de6d239
Pranav Vashi <neobuddy89@gmail.com>
2020-01-12 00:37:57 +0530

    msm8998-common: Skip interfaces that are buildable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -161,8 +161,7 @@ PRODUCT_PACKAGES += \
     libshim_camera \
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
@@ -203,8 +202,7 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_PACKAGES += \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    vendor.display.config@1.0
 
 # Doze
 PRODUCT_PACKAGES += \
