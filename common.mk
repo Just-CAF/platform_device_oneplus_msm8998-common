d15f68d54575cf0ddfbb943b3f7d01d1f2b13ea8
Pranav Vashi <neobuddy89@gmail.com>
2020-04-16 23:58:35 +0530

    msm8998-common: Use 6GB dalvik-heap config
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -29,7 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 -include  vendor/custom/config/common_full_phone.mk
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
