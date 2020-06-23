088d29c12d261eec002a69b9bfe7e208ab513e39
trautamaki <timi.rautamaki@gmail.com>
2020-06-23 06:55:48 +0200

    msm8998-common: use common AptX blobs

--- common.mk
+++ common.mk
@@ -170,6 +170,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0
 
+# Bluetooth aptX(HD)
+include vendor/qcom/common/bt/qti-bt.mk
+
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
