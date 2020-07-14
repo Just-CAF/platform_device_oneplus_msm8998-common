97bca8522bfd1281a524dc4f0e711e91eaaa70f6
trautamaki <timi.rautamaki@gmail.com>
2020-07-14 13:09:47 +0200

    msm8998-common: build btconfigstore

--- common.mk
+++ common.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
     libbluetooth_qti \
     vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0
 
